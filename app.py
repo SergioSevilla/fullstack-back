b84fd612d33d0aa1fbcf1d66488eb144e7d3ddff
SergioSevilla <bitgenius@hotmail.com>
2023-01-05 11:00:56 +0100

    Correcciones código estático

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
+""" API Pizzas FullStack"""
 from flask import Flask,request,redirect
 from persistencia import guardar_pedido
 
 app = Flask(__name__)
 
 @app.route("/pizza",methods=['POST'])
-#Registramos los parámetros que nos vienen por POST de prepara_pedido. 
 def registrar_pedido():
-  nombre = request.form.get ("fname")
-  apellidos = request.form.get ("surname")
-  print("Nombre : "+nombre+" , Apellidos: "+apellidos)
-  guardar_pedido(nombre,apellidos)
-  return redirect("http://localhost/solicita_pedido.html", code=302) 
+    """ Registramos los parámetros que nos vienen por POST de prepara_pedido. """
+    nombre = request.form.get ("fname")
+    apellidos = request.form.get ("surname")
+    print("Nombre : "+nombre+" , Apellidos: "+apellidos)
+    guardar_pedido(nombre,apellidos)
+    return redirect("http://localhost/solicita_pedido.html", code=302)
--- persistencia.py
+++ persistencia.py
@@ -1,8 +1,7 @@
+""" Persistencia de Pizza Full Stack"""
+
 def guardar_pedido(nombre, apellidos):
-  ''' Abre el fichero pedidos.txt en modo append para agregar la 
-      dupla <nombre,apellidos> como un pedido. Una vez escrito en 
-      el fichero, se cierra. '''
-      
-  with open("pedidos.txt", "a", encoding="utf-8") as file:  
-    file.write(nombre + " " + apellidos + "\n")             #Cada línea será un pedido.
-    file.close()
+    """ Abre el fichero pedidos.txt para agregar la dupla <nombre,apellidos> como un pedido. """
+    with open("pedidos.txt", "a", encoding="utf-8") as file:
+        file.write(nombre + " " + apellidos + "\n")             #Cada línea será un pedido.
+        file.close()
